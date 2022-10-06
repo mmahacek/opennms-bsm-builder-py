76ae57f6508601cceb3ed25adf4fde43b10b2009
Mark Mahacek <mmahacek@opennms.com>
2022-10-06 16:25:00 -0700

    update modeling example

--- bsm_builder.py
+++ bsm_builder.py
@@ -13,6 +13,9 @@ from pyonms.models.business_service import (
     IPServiceEdgeRequest,
     ChildEdgeRequest,
     MapFunction,
+    ReduceFunction,
+    Attribute,
+    Severity,
 )
 
 load_dotenv()
@@ -32,7 +35,7 @@ def get_bsm_list(all_bsms):
     bsm_list = {}
     for node in nodes:
         match = re.match(
-            "MI_(?P<org>.*)-(?P<host>.*)(?P<instance>0[1-2])(?P<function>S|VMR|DNFVI)",
+            "MI_(?P<org>.*)-(?P<host>.*)(?P<instance>[0-9][0-9])(?P<function>S|VMR|DNFVI|-vFW)",
             node.label,
         )
         if match:
@@ -62,18 +65,22 @@ def get_bsm_list(all_bsms):
 
 
 if __name__ == "__main__":  # noqa: C901
+    overall_progress = tqdm(desc="Overall progress", unit="step", total=3)
     all_bsms = my_server.bsm.get_bsms()
+
+    overall_progress.update(1)
 
     bsm_list = get_bsm_list(all_bsms)
 
-    site_bsms = {}
+    overall_progress.update(1)
 
+    site_bsms = {}
     used_services = []
     with tqdm(
         unit="site", desc="Updating BSM models", total=len(bsm_list.keys())
-    ) as pbar:
+    ) as progress_bar:
         for group, data in bsm_list.items():
-            pbar.set_description(f"Updating {group} BSM model")
+            progress_bar.set_description(f"Updating {group} BSM model")
             if not site_bsms.get(data["parent"]):
                 site_bsms[data["parent"]] = my_server.bsm.find_bsm_name(
                     name=data["parent"]
@@ -87,11 +94,8 @@ if __name__ == "__main__":  # noqa: C901
                 new_bsm = switch_bsm.request()
             else:
                 new_bsm = BusinessServiceRequest(name=group)
-            virtual_bsm = my_server.bsm.find_bsm_name(name=f"{group}-VNF")
-            if virtual_bsm:
-                vnf_bsm = virtual_bsm.request()
-            else:
-                vnf_bsm = BusinessServiceRequest(name=f"{group}-VNF")
+            new_bsm.add_attribute(Attribute(key="site", value=data["parent"]))
+            new_bsm.add_attribute(Attribute(key="model", value="device"))
             for node in data["nodes"]:
                 for ip in node["node"].ipInterfaces:
                     for service in ip.services:
@@ -102,43 +106,60 @@ if __name__ == "__main__":  # noqa: C901
                             friendly_name = f"{group}{node['function']}-{service.serviceType.name}".replace(
                                 "Flexware-", ""
                             )
+                            friendly_name = (
+                                node["function"]
+                                .replace("-", "")
+                                .replace("S", "SAOS")
+                                .replace("VMR", "Vyatta")
+                                .replace("vFW", "Velo")
+                            )
 
                             if node["function"] == "S":
                                 new_bsm.update_edge(
                                     ip_edge=IPServiceEdgeRequest(
                                         friendly_name=friendly_name,
                                         ip_service_id=service.id,
+                                        map_function=MapFunction(
+                                            type="SetTo", status=Severity.CRITICAL
+                                        ),
+                                    )
+                                )
+                                used_services.append(service)
+                            elif node["function"] == "DNFVI":
+                                new_bsm.update_edge(
+                                    ip_edge=IPServiceEdgeRequest(
+                                        friendly_name=friendly_name,
+                                        ip_service_id=service.id,
+                                        map_function=MapFunction(
+                                            type="SetTo",
+                                            status=Severity.MAJOR,
+                                        ),
+                                    )
+                                )
+                                used_services.append(service)
+                            elif node["function"] == "VMR":
+                                new_bsm.update_edge(
+                                    ip_edge=IPServiceEdgeRequest(
+                                        friendly_name=friendly_name,
+                                        ip_service_id=service.id,
+                                        map_function=MapFunction(
+                                            type="SetTo",
+                                            status=Severity.MINOR,
+                                        ),
                                     )
                                 )
                                 used_services.append(service)
                             else:
-                                vnf_bsm.update_edge(
+                                new_bsm.update_edge(
                                     ip_edge=IPServiceEdgeRequest(
                                         friendly_name=friendly_name,
                                         ip_service_id=service.id,
+                                        map_function=MapFunction(
+                                            type="SetTo", status=Severity.WARNING
+                                        ),
                                     )
                                 )
                                 used_services.append(service)
-            if virtual_bsm:
-                new_vnf_bsm = my_server.bsm.update_bsm(bsm=vnf_bsm, id=virtual_bsm.id)
-            else:
-                new_vnf_bsm = my_server.bsm.create_bsm(vnf_bsm)
-            data["new"] = new_vnf_bsm
-            if new_vnf_bsm.id not in [bsm.id for bsm in all_bsms]:
-                all_bsms.append(new_vnf_bsm)
-            child = [bsm for bsm in all_bsms if bsm.id == new_vnf_bsm.id]
-            if child:
-                new_bsm.child_edges.append(
-                    ChildEdgeRequest(
-                        child_id=child[0].id, map_function=MapFunction("Decrease")
-                    )
-                )
-            else:
-                new_bsm.child_edges.append(
-                    ChildEdgeRequest(
-                        child_id=new_vnf_bsm.id, map_function=MapFunction("Decrease")
-                    )
-                )
             if switch_bsm:
                 new_parent_bsm = my_server.bsm.update_bsm(bsm=new_bsm, id=switch_bsm.id)
             else:
@@ -147,14 +168,19 @@ if __name__ == "__main__":  # noqa: C901
             if new_parent_bsm not in [bsm.id for bsm in all_bsms]:
                 all_bsms.append(new_parent_bsm)
             site_bsm_update = site_bsms[data["parent"]].request()
+            site_bsm_update.add_attribute(Attribute(key="site", value=data["parent"]))
+            site_bsm_update.add_attribute(Attribute(key="model", value="site"))
+            site_bsm_update.reduce_function = ReduceFunction(type="HighestSeverity")
             site_bsm_update.update_edge(
                 child_edge=ChildEdgeRequest(
-                    child_id=new_parent_bsm.id, map_function=MapFunction(type="Ignore")
+                    child_id=new_parent_bsm.id,
+                    map_function=MapFunction(type="Decrease"),
                 )
             )
             site_bsms[data["parent"]] = my_server.bsm.update_bsm(
                 site_bsms[data["parent"]].id, site_bsm_update
             )
-            pbar.update(1)
+            progress_bar.update(1)
     my_server.bsm.reload_bsm_daemon()
+overall_progress.update(1)
 pass
